f99515b2e793c56a24e18a3fa36fcb384651292c
virgantara <vinux.edu@gmail.com>
2025-07-19 10:00:30 +0700

    update

--- main.py
+++ main.py
@@ -65,7 +65,7 @@ st_time = time.time()
 
 for epoch in tqdm(range(epochs)):
     train_loss = []
-    for batch in train_loader:
+    for batch in tqdm(train_loader):
         for item in batch:
             batch[item] = batch[item].to(DEVICE)
         loss_dict = posecnn_model(batch)
