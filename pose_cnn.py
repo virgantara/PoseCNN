3c22dab7740bc8a0f65ce16ed5ed5e9d3361eb98
virgantara <vinux.edu@gmail.com>
2025-07-20 18:30:24 +0700

    update

--- pose_cnn.py
+++ pose_cnn.py
@@ -366,12 +366,8 @@ class RotationBranch(nn.Module):
         # TODO: Implement forward pass of rotation branch.                   #
         ######################################################################
         # Replace "pass" statement with your code
-        print("feature1:",feature1.shape)
-        print("feature2:",feature2.shape)
         out1 = self.roi1(feature1, bbx.to(dtype = torch.float32))
         out2 = self.roi2(feature2, bbx.to(dtype = torch.float32))
-        print("out1:",out1.shape)
-        print("out2:",out2.shape)
         out = out1 + out2
         quaternion = self.lin2(self.relu_lin(self.lin1(out.flatten(1))))
 
